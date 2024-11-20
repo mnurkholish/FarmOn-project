4abef2918501f60ef16140ad449560a77be927b3
Nur Kholish <kholish.nur8306@gmail.com>
2024-11-20 13:37:55 +0700

    menambah fitur tambah_produk dan hapus_produk

--- produk.py
+++ produk.py
@@ -35,6 +35,40 @@ def lihat_produk(jenis):
         print(f"|{i+1:>2}.| {nama:<18}| {satuan:<8}| Rp{harga:<15}|")
     print("+---+" + "-"*19 + "+" + "-"*9 + "+" + "-"*18 + "+")
 
+def tambah_produk_baru(jenis, nama, satuan, harga, stok):
+    '''tambah produk'''
+    df = pd.read_csv("data_produk.csv")
+
+    if (jenis in df["jenis"].values) and (nama in df["nama"].values):
+        print("produk sudah ada")
+    else:
+        data = {
+            "jenis" : [jenis],
+            "nama" : [nama],
+            "satuan" : [satuan],
+            "harga" : [harga],
+            "stok" : [stok]
+        }
+        produk_baru = pd.DataFrame(data)
+        df = pd.concat([df, produk_baru], ignore_index=True)
+        df = df.sort_values(by=["jenis", "nama"])
+        df.to_csv("data_produk.csv", index=False)
+        print("Berhasil ditambahkan")
+
+def hapus_produk(jenis, nama):
+    '''hapus produk'''
+    df = pd.read_csv("data_produk.csv")
+
+    if (jenis in df["jenis"].values) and (nama in df["nama"].values):
+        index_produk = df[(df["jenis"] == jenis) & (df["nama"] == nama)].index
+        df.drop(index=index_produk, inplace=True)
+        df.to_csv("data_produk.csv", index=False)
+        print("produk berhasil dihapus")
+    else:
+        print("produk tidak ada")
+
+hapus_produk("buah", "rambutan")
+
 def main():
     '''run'''
     while True:
