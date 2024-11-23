873f22263a6d4b6df9c1d0f0b031ea3c48aaa460
Nur Kholish <kholish.nur8306@gmail.com>
2024-11-23 13:46:56 +0700

    rombak main

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def header(judul=None):
     # clear terminal
     os.system("cls")
 
+    # header
     width = 100
-    print("="*width)
+    print("=" * width)
     print("FarmOn".center(width))
-    print("="*width)
+    print("=" * width)
+    if judul is not None:
+        print(judul.center(width))
+    print("-" * width)
 
 def intro():
     '''tampilan pertama aplikasi'''
@@ -19,88 +23,97 @@ def intro():
     width = 100
     print("\n\n" + "Selamat Datang di aplikasi FarmOn".center(width))
     print("Tekan enter untuk memulai aplikasi".center(width) + "\n\n")
-    input("="*width + "\n")
+    print("-"*width + "\n" + "=" * width)
+    input()
+
+def outro():
+    """Tampilan penutup aplikasi"""
+    header()
+    width = 100
+    print("\n\n" + "Terima Kasih Telah Menggunakan Aplikasi FarmOn :)".center(width))
+    print("Tekan enter untuk menutup aplikasi".center(width) + "\n\n")
+    print("-"*width + "\n" + "=" * width)
+    input()
 
 def registrasi():
     '''registrasi'''
     while True:
-        header()
-        x = "registrasi |"
-        print(x + "\n" + "-"*(len(x)-1) + "+")
+        header("Registrasi")
 
-        username = input("username: ")
-        password = input("password: ")
-        confirm_password = input("confirm password: ")
+        username = input("username: ").strip()
+        password = input("password: ").strip()
         role = "user"
 
-        if confirm_password != password:
-            print("password tidak sama silahkan coba lagi")
-            input("Tekan enter untuk mengulang")
+        # validasi username harus ada
+        if not username:
+            print("Username tidak boleh kosong")
+            input("Tekan enter untuk melanjutkan")
             continue
+        # validasi password harus ada
+        if not password:
+            print("Password tidak boleh kosong")
+            input("Tekan enter untuk melanjutkan")
+            continue
+        # validasi password harus berupa huruf dan angka
+        if password:
+            huruf = False
+            angka = False
+            for karakter in password:
+                if karakter.isalpha():
+                    huruf = True
+                if karakter.isnumeric():
+                    angka = True
+            
+            if not (huruf and angka):
+                print("Password harus berupa huruf dan angka")
+                input("Tekan untuk melanjutkan")
+                continue
 
-        df = pd.read_csv("akun_pengguna.csv")
+        data_akun = "akun_pengguna.csv"
+        df = pd.read_csv(data_akun)
+
+        # validasi username harus berbeda dari yang sudah ada
         if username in df["username"].values:
             print("username sudah ada, gunakan username yang berbeda")
             input("tekan enter untuk mengulang")
             continue
+
+        # jika syarat terpenuhi, membuat data baru
+        user_baru = pd.DataFrame([[username, password, role]], columns=["username", "password", "role"])
+
+        # mengupdate data
+        df = pd.concat([df, user_baru], ignore_index=True)
+        df.to_csv(data_akun, index=False)
+
+        print("Akun berhasil dibuat. Silahkan login ulang!")
+        input("Tekan enter untuk melanjutkan")
         break
-
-    user_baru = pd.DataFrame([[username, password, role]], columns=["username", "password", "role"])
-
-    df = pd.concat([df, user_baru], ignore_index=True)
-    df.to_csv("akun_pengguna.csv", index=False)
-
-    print("Akun berhasil dibuat. Silahkan login ulang!")
-    input("Tekan enter untuk melanjutkan")
 
 def login():
     '''Login'''
-    header()
-    x = "LOGIN |"
-    print(x + "\n" + "-"*(len(x)-1) + "+")
+    while True:
+        header("Login")
 
-    username = input("Username: ")
-    password = input("Password: ")
+        username = input("Username: ")
+        password = input("Password: ")
 
-    global user # pylint: disable=global-variable-undefined
-    user = username
+        global user # pylint: disable=global-variable-undefined
+        user = username
 
-    df = pd.read_csv("akun_pengguna.csv")
+        df = pd.read_csv("akun_pengguna.csv")
 
-    ambil_role = df[(df["username"] == username) & (df["password"] == password)]
+        ambil_role = df[(df["username"] == username) & (df["password"] == password)]
 
-    if not ambil_role.empty:
-        role = ambil_role.iloc[0,2]
+        if ambil_role.empty:
+            role = "penyusup"
+        else:
+            role = ambil_role.iloc[0]['role']
+        
         return role
 
 def main():
     '''program utama'''
-    while True:
-        intro()
-        while True:
-            header()
-            print("Pilih opsi\n1. Login\n2. registrasi\n0. Exit")
-            opsi = input("Masukkan opsi pilihan sesuai angka (1/2/3) >")
-            if opsi == "1":
-                role = login()
-                if role == "user":
-                    print("kamu adalah user")
-                    input()
-                elif role == "admin":
-                    print("kamu adalah admin")
-                    input()
-                else:
-                    print("Login gagal, username tidak ditemukan")
-                    print("Silahkan pilih opsi registrasi jika anda belum punya akun")
-                    input("Tekan enter untuk melanjutkan")
-                    continue
-
-            elif opsi == "2":
-                registrasi()
-
-            elif opsi == "0":
-                print("Terima kasih")
-                input()
-                return
+    intro()
+    outro()
 
 main()
